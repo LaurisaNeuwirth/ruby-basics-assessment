c8bb3887c907563a18112abedbfb153c40d9e8a3
LaurisaNeuwirth <uberlaurisa@gmail.com>
2015-10-20 09:34:52 -0400

    finished quiz mostly

--- challenge.rb
+++ challenge.rb
@@ -8,6 +8,10 @@
 33
 65.89
 
+"Hello World"
+33
+65.89
+
 # Question 2: Booleans
 # What would be the result of executing the below statements
 
@@ -17,18 +21,27 @@ else
   puts "it's false"
 end
 
+result = true
+
 # 2.1 Substitue "hello" && 4.5 for 33 && 55 above. What's the output?
+result = true
 # 2.2 Substitue nil && 88 for 33 && 55 above. What's the output?
+result = false
 # 2.4 Substitue 63.4 < 64. What's the output?
+result = true
 
 # Question 3 :Strings
 
 # How would you (Yes, you can look up the String methods!):
 
 # 3.1. determine if a string is included another another string?
+.include?
+
 # 3.2. determine if the length of a string?
+.length
 # 3.3. Go from the string "eels in my hovercraft" to "fish in my sink" but don't
 # change the string "eels in my hovercraft"
+.gsub, not using the !
 
 
 # Question 4 : Variables
@@ -38,9 +51,15 @@ end
 vehicle = "4 wheel Bus"
 
 # 4.1 to "6 wheel Bus"
+
+vehicle = "6 wheel Bus"
+
 # 4.2 to "6 wheel tank"
+vehicle = "6 wheel tank"
+
 # 4.3 Create a NEW string from the string in 4.2 that will be
 # "6 wheel tank that shoots rainbow bunnnies"
+vehicle +=  "  that shoots rainbow bunnnies"
 
 # Question 5 : Loop
 # - write a loop that guesses your age.
@@ -51,8 +70,43 @@ vehicle = "4 wheel Bus"
 # - I'm not kidding, say unkind works to a user that thinks your 10 years older
 #   than you are.
 
+my_age = 29
+
+# keep track of how many guesses user is taking
+this_turn = 1
+
+#only allow 4 guesses
+while this_turn <= 4
+  print "How old do you think I am? "
+
+  #take input from user
+  user_guess = gets.chomp!.to_i
+
+  if user_guess == my_age
+    puts "You are correct!"
+    break
+  elsif user_guess >= (my_age + 10)
+    # If user guesses me to be 10 years or more older than I am
+
+    print "WTF you are not my friend!"
+    #increment this turn count by 1 each time
+    this_turn += 1
+  else
+    print "Wrong! Guess again!"
+    #increment counter and loop again
+    this_turn += 1
+  end
+
+  if this_turn == 5
+    print "You ran out of guesses! Too bad!"
+  end
+
+end
+
+
 # Bonus Question: Who said this?
 
 # The great enemy of the truth is very often not the lie -- deliberate, contrived
 # and dishonest, but the myth, persistent, persuasive, and unrealistic.
 # Belief in myths allows the comfort of opinion without the discomfort of thought.
+JFK at Yale, 1962 I googled it bc I wanted to know.
